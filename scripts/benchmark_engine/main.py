38d8fe670116db92c6971b349a3f522aff207c08
vdkjg <hweichelt@uni-potsdam.de>
2021-01-18 16:35:04 +0100

    + Benchmark 6 New + Benchmark Engine Pyhthon created

---
+++ scripts/benchmark_engine/main.py
@@ -0,0 +1,5 @@
+from benchmark_engine import Benchmark
+
+# if __name__ == '__main__':
+
+b1 = Benchmark("/home/hannes/Programming/PlanMerger/instances/benchmarks/1_vertex_constraint_benchmark [Level 1]")
